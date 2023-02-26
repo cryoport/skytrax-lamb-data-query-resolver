0d9229783fe9226469b440a3985d06d439644851
Armando Prieto <aprieto@cryoport.com>
2023-02-26 13:38:50 -0800

    fix warning

--- app/src/main/java/com/cryoport/skytrax/resolver/resolvers/Resolver.java
+++ app/src/main/java/com/cryoport/skytrax/resolver/resolvers/Resolver.java
@@ -4,7 +4,7 @@ import com.cryoport.skytrax.resolver.model.dto.ResponseType;
 
 import java.util.Map;
 
-public interface Resolver<T extends ResponseType> {
+public interface Resolver<T extends ResponseType<?>> {
 
      T resolve(Map<String, Object> event);
 
